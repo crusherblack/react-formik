b5943327ae35f567ad24d234df2e8eb1d9b97989
fadhil darma <fadhildarma13@gmail.com>
2020-07-09 23:08:04 +0700

    fix array formik

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, FieldArray, getIn } from "formik";
+import * as yup from "yup";
 
 function App() {
+  const validationSchema = yup.object({
+    firstName: yup.string().required().max(100).min(5),
+    pets: yup.array().of(
+      yup.object({
+        name: yup.string().required(),
+      })
+    ),
+  });
+
   return (
     <div className="container mt-5">
       <h1 className="text-center">React Learn Formik</h1>
@@ -12,7 +22,18 @@ function App() {
           isTall: "",
           cookies: [],
           yogurt: "",
+          pets: [
+            {
+              name: "jarvis",
+              type: "dog",
+            },
+            {
+              name: "soma",
+              type: "cat",
+            },
+          ],
         }}
+        validationSchema={validationSchema}
         onSubmit={(data, { setSubmitting, resetForm }) => {
           /* for async */
           setSubmitting(true);
@@ -23,18 +44,31 @@ function App() {
           resetForm();
         }}
       >
-        {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
+        {({
+          values,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          errors,
+          touched,
+        }) => (
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="firstName">First Name</label>
               <input
                 type="text"
-                className="form-control"
+                className={`form-control ${
+                  touched.firstName && errors.firstName ? "is-invalid" : null
+                } `}
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.firstName}
                 name="firstName"
               />
+              <small className="text-danger">
+                {touched.firstName && errors.firstName}
+              </small>
             </div>
             <div className="form-group">
               <label htmlFor="lastName">Last Name</label>
@@ -121,11 +155,69 @@ function App() {
                 </label>
               </div>
             </div>
+            <div className="form-group">
+              <FieldArray name="pets">
+                {(arrayHelpers) => (
+                  <div>
+                    <button
+                      type="button"
+                      className="btn btn-info btn-block"
+                      onClick={() =>
+                        arrayHelpers.push({
+                          name: "",
+                          type: "",
+                        })
+                      }
+                    >
+                      Add Pet
+                    </button>
+                    {values.pets.map((pet, index) => {
+                      const inputPetName = `pets.${index}.name`;
+                      const selectTypeName = `pets.${index}.type`;
+                      const error = getIn(errors, inputPetName);
+
+                      return (
+                        <div key={index}>
+                          <input
+                            type="text"
+                            className="form-control mt-2"
+                            placeholder="Pet name"
+                            name={inputPetName}
+                            value={pet.name}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                          />
+                          <select
+                            name={selectTypeName}
+                            className="form-control mt-2"
+                            value={pet.type}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                          >
+                            <option value="">Select Pet</option>
+                            <option value="cat">cat</option>
+                            <option value="dog">dog</option>
+                            <option value="frog">frog</option>
+                          </select>
+                          <button
+                            className="btn btn-danger btn-block mt-2"
+                            onClick={() => arrayHelpers.remove(index)}
+                          >
+                            Remove
+                          </button>
+                          <small className="text-danger">{error}</small>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </FieldArray>
+            </div>
 
             <div className="form-group">
               <button
                 type="submit"
-                className="btn btn-primary"
+                className="btn btn-primary btn-block"
                 disabled={isSubmitting}
               >
                 Submit Form
@@ -133,6 +225,7 @@ function App() {
             </div>
 
             <pre>{JSON.stringify(values, null, 2)}</pre>
+            <pre>{JSON.stringify(errors, null, 2)}</pre>
           </form>
         )}
       </Formik>
